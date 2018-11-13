58b1d2bee58c28731775cbd550df0a60ea1c4b83
praveengatti <praveengatti123@gmail.com>
2018-11-13 16:32:03 +0530

    Added Ui to the gameplay

---
+++ Assets/Scripts/UiController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UiController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void LoadScene(string s)
+    {
+        SceneManager.LoadScene(s);
+    }
+
+    public void PanelOn(GameObject Panel)
+    {
+        Panel.gameObject.SetActive(true);
+    }
+
+    public void ClosePanel(GameObject Panel)
+    {
+        Panel.gameObject.SetActive(false);
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void resume()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void MuteAndUmute()
+    {
+        SoundManager.Instance.ToggleMute();
+
+        if (SoundManager.Instance.IsMuted())
+        {
+            GameObject.Find("Mute").GetComponentInChildren<Text>().text = "UnMute";
+        }
+        else
+        {
+            GameObject.Find("Mute").GetComponentInChildren<Text>().text = "Mute";
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
