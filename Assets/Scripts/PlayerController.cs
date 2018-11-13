b42dcf2ac700af6ba55b42b4dbfb42e8abe5d089
killamkiller <santoshkillamsetty10@gmail.com>
2018-11-13 14:45:07 +0530

    charcter controller and animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,103 +4,33 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    public float MoveDistance = 150;
 
-    public float speed;        
+    private int playerPosition = 1;
 
-    private Rigidbody2D rb2d;      
-
-    public Button LeftButton;
-    public Button RightButton;
-
-    float moveHorizontal;
-    bool IsCalled;
-
-    public int InGameJunkCount;
-    public Text InGameJunkCountText;
-
-    public  bool gameover;
-
-    public static PlayerController Instance;
-    void Awake()
+    private void Update()
     {
-        if (Instance)
+        // if D key is pressed down
+        if (Input.GetKeyDown(KeyCode.D))
         {
-            DestroyImmediate(gameObject);
+            if (playerPosition < 2)
+            {
+                // move the player along the positive Z axis
+                transform.Translate(MoveDistance,0,0);
+                // increment the player position
+                playerPosition++;
+            }
         }
-        else
+        // if A key is pressed down
+        else if (Input.GetKeyDown(KeyCode.A))
         {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-    }
-        void Start()
-    {
-        rb2d = GetComponent<Rigidbody2D>();
-
-        Button Left = LeftButton.GetComponent<Button>();
-        Button right = RightButton.GetComponent<Button>();
-    
-        Left.onClick.AddListener(MoveLeft);
-        right.onClick.AddListener(MoveRight);
-
-        IsCalled = false;
-        gameover = false;
-
-        InGameJunkCount = 0;
-    }
-
-    void FixedUpdate()
-    {
-        
-        Vector2 movement = new Vector2(moveHorizontal, 0f);
-        rb2d.AddForce(movement * speed);
-        if(IsCalled)
-        {
-            IsCalled = false;
-            StartCoroutine(Example());
-        }
-    }
-
-    public void MoveLeft()
-    {
-        moveHorizontal = -1f;
-        IsCalled = true;        
-    }
-
-    public void MoveRight()
-    {
-        moveHorizontal = 1f;
-        IsCalled = true;
-    }
-
-    IEnumerator Example()
-    {
-        yield return new WaitForSeconds(2);
-        moveHorizontal = 0f;
-    }
-
-    public void UpdateJunkValue()
-    {
-        InGameJunkCount++;
-        InGameJunkCountText.text = "" + InGameJunkCount;
-
-    }
-
-    public void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Obstacle")
-        {
-
-            Time.timeScale = 0;
-             gameover = true;
-
-          //  this.gameObject.SetActive(false);
-        }
-        if (collision.gameObject.tag == "Junk")
-        {
-            UpdateJunkValue();
-            // Destroy(collision.gameObject);
-            collision.gameObject.SetActive(false);
+            if (playerPosition > 0)
+            {
+                // move the player along the negative Z axis
+                transform.Translate(-MoveDistance,0, 0);
+                // decrement the player position
+                playerPosition--;
+            }
         }
     }
 }
