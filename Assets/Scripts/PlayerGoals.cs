4f4535b301f77ad6e30e2311cc64203c4aa4420d
killamkiller <santoshkillamsetty10@gmail.com>
2018-11-13 19:47:04 +0530

    playerGoals

---
+++ Assets/Scripts/PlayerGoals.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerGoals : MonoBehaviour {
+
+    public int MaxIncomeEquality;
+    public int MaxGenderEquality;
+    public int MaxEducationEquality;
+    public int MaxReligionEquality;
+
+    public Slider IncomeSlider;
+    public Slider GenderSlider;
+    public Slider EducationSlider;
+    public Slider ReligionSlider;
+
+    public static PlayerGoals Instance ;
+
+    private void Awake()
+    {
+        if (Instance)
+        {
+            DestroyImmediate(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        MaxIncomeEquality = (int)Random.Range(70, 90);
+        MaxGenderEquality = (int)Random.Range(70, 90);
+        MaxReligionEquality = (int)Random.Range(70, 90);
+        MaxEducationEquality = (int)Random.Range(70, 90);
+
+
+        IncomeSlider.maxValue = 100;
+        IncomeSlider.minValue = 0;
+        IncomeSlider.value = MaxIncomeEquality;
+
+        GenderSlider.maxValue = 100;
+        GenderSlider.minValue = 0;
+        GenderSlider.value = MaxGenderEquality;
+
+        EducationSlider.maxValue = 100;
+        EducationSlider.minValue = 0;
+        EducationSlider.value = MaxEducationEquality;
+
+        ReligionSlider.maxValue = 100;
+        ReligionSlider.minValue = 0;
+        ReligionSlider.value = MaxReligionEquality;
+
+    }
+	
+	
+}
