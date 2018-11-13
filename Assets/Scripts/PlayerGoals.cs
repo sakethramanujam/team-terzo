6a8020cf73be58ffa80e0cdc5df02b2bd2348127
killamkiller <santoshkillamsetty10@gmail.com>
2018-11-14 00:41:46 +0530

    bug fixes

--- Assets/Scripts/PlayerGoals.cs
+++ Assets/Scripts/PlayerGoals.cs
@@ -11,15 +11,7 @@ public class PlayerGoals : MonoBehaviour {
     public int MaxEducationEquality;
     public int MaxReligionEquality;
 
-    public Slider IncomeSlider;
-    public Slider GenderSlider;
-    public Slider EducationSlider;
-    public Slider ReligionSlider;
 
-    public Text Educate;
-    public Text Economy;
-    public Text religion;
-    public Text Gender;
 
     public static PlayerGoals Instance ;
 
@@ -46,28 +38,7 @@ public class PlayerGoals : MonoBehaviour {
         MaxReligionEquality = (int)Random.Range(70, 90);
         MaxEducationEquality = (int)Random.Range(70, 90);
 
-        Educate.text = "" + MaxEducationEquality+"%";
-        Economy.text = "" + MaxIncomeEquality + "%";
-        religion.text = "" + MaxReligionEquality + "%";
-        Gender.text = "" + MaxGenderEquality + "%";
-
-
-        IncomeSlider.maxValue = 100;
-        IncomeSlider.minValue = 0;
-        IncomeSlider.value = MaxIncomeEquality;
-
-        GenderSlider.maxValue = 100;
-        GenderSlider.minValue = 0;
-        GenderSlider.value = MaxGenderEquality;
-
-        EducationSlider.maxValue = 100;
-        EducationSlider.minValue = 0;
-        EducationSlider.value = MaxEducationEquality;
-
-        ReligionSlider.maxValue = 100;
-        ReligionSlider.minValue = 0;
-        ReligionSlider.value = MaxReligionEquality;
-
+      
     }
 
     IEnumerator Loadscene()
--- Assets/Scripts/SliderContainer.cs
+++ Assets/Scripts/SliderContainer.cs
@@ -1,16 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SliderContainer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    public Slider IncomeSlider;
+    public Slider GenderSlider;
+    public Slider EducationSlider;
+    public Slider ReligionSlider;
+
+    public Text Educate;
+    public Text Economy;
+    public Text religion;
+    public Text Gender;
+    // Use this for initialization
+    void Start () {
+
+
+        Educate.text = "" +PlayerGoals.Instance.MaxEducationEquality + "%";
+        Economy.text = "" + PlayerGoals.Instance.MaxIncomeEquality + "%";
+        religion.text = "" + PlayerGoals.Instance.MaxReligionEquality + "%";
+        Gender.text = "" + PlayerGoals.Instance.MaxGenderEquality + "%";
+
+
+        IncomeSlider.maxValue = 100;
+        IncomeSlider.minValue = 0;
+        IncomeSlider.value = PlayerGoals.Instance.MaxIncomeEquality;
+
+        GenderSlider.maxValue = 100;
+        GenderSlider.minValue = 0;
+        GenderSlider.value = PlayerGoals.Instance.MaxGenderEquality;
+
+        EducationSlider.maxValue = 100;
+        EducationSlider.minValue = 0;
+        EducationSlider.value = PlayerGoals.Instance.MaxEducationEquality;
+
+        ReligionSlider.maxValue = 100;
+        ReligionSlider.minValue = 0;
+        ReligionSlider.value = PlayerGoals.Instance.MaxReligionEquality;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
