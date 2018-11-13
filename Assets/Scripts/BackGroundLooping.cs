397c94424cf745b416b8a195f79c94a5fbe3a66f
killamkiller <santoshkillamsetty10@gmail.com>
2018-11-13 15:36:48 +0530

    added background and object pooling

---
+++ Assets/Scripts/BackGroundLooping.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundLooping: MonoBehaviour
+{
+    public float bgLayerSpeed;
+    Transform bgCameraTransform;
+   
+    public GameObject bgPrevious;
+    public GameObject bgCurrent;
+    public GameObject bgNext;
+
+    float bgDeltaX;
+    float bgLastCameraX;
+
+    public float bgSize;
+    float ViewZone = 10;
+    int LeftIndex;
+    int RightIndex;
+
+
+    private void Start()
+    {
+        bgCameraTransform = Camera.main.transform;
+        bgLastCameraX = bgCameraTransform.position.y;
+        bgCameraTransform = Camera.main.transform;
+    }
+
+    private void Update()
+    {
+        bgDeltaX = bgCameraTransform.position.y - bgLastCameraX;
+        transform.position += Vector3.down * (bgDeltaX * bgLayerSpeed);
+        bgLastCameraX = bgCameraTransform.position.y;
+        
+        if (bgCameraTransform.position.y > (bgNext.transform.position.y))
+        {
+            bgScrollRight();
+            Debug.Log("scrolling right");
+        }
+    }
+
+
+
+
+
+   
+
+    public void bgScrollRight()
+    {
+        GameObject tempcurrent;
+        tempcurrent = bgPrevious;
+        bgPrevious = bgCurrent;
+        bgCurrent = bgNext;
+        bgNext = tempcurrent;
+        //do
+        //{
+        //    bgNext = gamemanger.RandomSelectionOfBg();
+
+        //} while (bgPrevious != bgNext &&bgCurrent != bgNext);
+
+        bgNext.transform.position = bgCurrent.transform.position + bgSize * Vector3.up;
+
+        
+    }
+
+}
---
+++ Assets/Scripts/MoveDownwards.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDownwards : MonoBehaviour {
+
+    public int Speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += Vector3.down *Speed* Time.deltaTime;
+    }
+}
