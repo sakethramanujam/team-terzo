12d1b06dce8f64c92dd685f33298ec806c58acad
praveengatti <praveengatti123@gmail.com>
2018-11-13 15:42:52 +0530

    Added SplashScreen

---
+++ Assets/Scripts/Splash.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Splash : MonoBehaviour
+{
+    public float Logotime = 2f;
+    public GameObject SplashScreen;
+
+    public void Loading()
+    {
+       
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+       // SoundManager.Instance.PlayMusic(SoundManager.Instance.GameSounds[0]);
+        StartCoroutine(ShowLogo());
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    IEnumerator ShowLogo()
+    {
+        yield return new WaitForSeconds(Logotime);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+ 
+
+}
